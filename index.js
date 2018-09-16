f709e430aa604058d9af2a6a3ce80a385ca58e16
Yujin Ariza <yujin.ariza@gmail.com>
2018-09-16 13:50:40 -0400

    make it work with latest glslify-loader

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-var escodegen = require('escodegen')
 var optimize  = require('./optimize')
-var through   = require('through2')
-var esprima   = require('esprima')
-var astw      = require('astw')
 
 module.exports = transform
 
@@ -11,70 +7,42 @@ if (process.env.DEBUG === 'glslify-optimize') {
   debug = console.error
 }
 
-function transform(filename) {
-  var stream = through(write, flush)
-  var buffer = []
+function transform(filename, src, options, nextPostTransform) {
 
-  return stream
-
-  function write(chunk, _, next) {
-    buffer.push(chunk)
-    next()
+  function err(reason) {
+    nextPostTransform(reason, null);
   }
 
-  function flush() {
-    var src = buffer.join('')
-    if (src.indexOf('glslify') === -1) {
-      this.push(src)
-      this.push(null)
-      return
-    }
+  function out(compiled) {
+    nextPostTransform(null, compiled);
+  }
 
-    var ast = esprima.parse(src)
-    var walk = astw(ast)
+  if (src.indexOf('GLSLIFY') === -1) {
+    return out(src);
+  }
 
-    debug('Original source:')
-    debug('\n' + src)
+  debug('Original source:');
+  debug('\n' + src);
 
-    walk(function(node) {
-      if (node.type !== 'Literal') return
-      if (node.value !== 'glslify/adapter.js') return
-      var parent = node
-      var callee = node
-      if (!(parent = parent.parent)) return
-      if (!(parent = parent.parent)) return
-      if (!(callee = parent.callee)) return
-      if (!(callee = callee.callee)) return
-      if (callee.name !== 'require') return
-      var args = parent.arguments
-      if (args.length < 2) return
-      if (args[0].type !== 'Literal') return
-      if (args[1].type !== 'Literal') return
-
-      var vert = args[0].value
-      var frag = args[1].value
-
-      debug('Original fragment shader:')
-      debug('\n' + frag)
-
-      debug('Original vertex shader:')
-      debug('\n' + vert)
-
-      frag = optimize.frag(frag)
-      vert = optimize.vert(vert)
-
-      debug('Optimized vertex shader:')
-      debug('\n' + vert)
-
+  var opti;
+  try {
+    if(src.indexOf('gl_FragColor') !== -1){
+      opti = optimize.frag(src);
       debug('Optimized fragment shader:')
-      debug('\n' + frag)
-
-      args[0].value = vert
-      args[1].value = frag
-    })
-
-    this.push(src = escodegen.generate(ast))
-    this.push(null)
-    debug(src)
+      debug('\n' + opti)
+      return out(opti);
+    }
+    else if(src.indexOf('gl_Position') !== -1){
+      opti = optimize.vert(src);
+      debug('Optimized vertex shader:')
+      debug('\n' + opti)
+      return out(opti);
+    }
+    else {
+      return out(src);
+    }
+  } catch (e) {
+    console.error('\x1b[31m', e);
+    return out(src);
   }
 }
